aa33f03058c6ba7f1e52039fd1041de3e6c10e20
higorj <higor_sgomes@outlook.com>
2022-05-13 21:51:03 -0300

    Fim da apostila 7

--- navegacao/lugaresNavigator.js
+++ navegacao/lugaresNavigator.js
@@ -13,7 +13,7 @@ const Stack = createNativeStackNavigator();
 const Container = (
     <NavigationContainer>
         <Stack.Navigator
-            initialRouteName="ListaDeLugares"
+            initialRouteName="NovoLugar"
             screenOptions={{
                 headerStyle: {
                     backgroundColor: Cores.primary
